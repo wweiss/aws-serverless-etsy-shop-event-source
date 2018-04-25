afd369ed309023acd4f30bffd0d0156a8cdec220
William Weiss <william@codification.org>
2018-04-24 22:20:00 -0500

    More packing cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 const gulp = require('gulp');
-const del = require('del');
-const zip = require('gulp-zip');
 const ts = require('gulp-typescript');
 const install = require('gulp-install');
 
@@ -16,16 +14,12 @@ gulp.task('watch', ['scripts'], () => {
   gulp.watch('src/**/*.ts', ['scripts']);
 });
 
-gulp.task('package-lambda', ['scripts'], function() {
+gulp.task('package', ['scripts'], function() {
   gulp
     .src('./package.json')
     .pipe(gulp.dest('lambda-dist/'))
     .pipe(install({ production: true }));
   gulp.src('dist/**').pipe(gulp.dest('lambda-dist/'));
-});
-
-gulp.task('clean-lambda', function() {
-  return del(['lambda-dist/']);
 });
 
 gulp.task('default', ['watch']);
