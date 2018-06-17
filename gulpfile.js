a6200ed7e92d1625c86e86a6ad667a9f788c7703
William Weiss <william@codification.org>
2018-06-17 12:09:34 -0500

    Updating Winston and the build process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,76 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
-const install = require('gulp-install');
+require('dotenv').config();
 
-// pull in the project TypeScript config
+const gulp = require('gulp');
+const del = require('del');
+const install = require('gulp-yarn');
+const ts = require('gulp-typescript');
+const run = require('gulp-run-command').default;
+
+const vars = {
+  stackName: process.env.STACK_NAME,
+  artifactBucket: process.env.ARTIFACT_BUCKET,
+};
+
+const paths = {
+  src: 'src',
+  dist: 'dist',
+  tmp: 'tmp'
+};
+
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
+const clean = () => {
+  return del([paths.dist, paths.tmp]);
+}
+exports.clean = clean;
+
+const configureTemp = () => {
+  return gulp
+    .src('app.template.yaml', {
+      read: false
+    })
+    .pipe(gulp.dest(paths.tmp));
+}
+exports.configureTemp = configureTemp;
+
+const transpileLambda = () => {
   const tsResult = tsProject.src().pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('dist'));
-});
+  return tsResult.js.pipe(gulp.dest(paths.dist));
+}
+exports.transpileLambda = transpileLambda;
 
-gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/**/*.ts', ['scripts']);
-});
+const packageLambda = () => {
+  return gulp.src(['./package.json', './yarn.lock'])
+    .pipe(gulp.dest(paths.dist))
+    .pipe(
+      install({
+        production: true
+      })
+    ).on('end', () => {
+      del([`${paths.dist}/package.json`, `${paths.dist}/*.lock`]);
+    });
+}
+exports.packageLambda = packageLambda;
 
-gulp.task('package', ['scripts'], function() {
-  gulp
-    .src('./package.json')
-    .pipe(gulp.dest('lambda-dist/'))
-    .pipe(install({ production: true }));
-  gulp.src('dist/**').pipe(gulp.dest('lambda-dist/'));
-});
+const packageCloudFormation = () => {
+  return run(`aws cloudformation package \
+  --template app.template.yaml \
+  --s3-bucket ${vars.artifactBucket} \
+  --output-template ${paths.tmp}/packaged-sam.yaml`)();
+}
+exports.packageCloudFormation = packageCloudFormation;
 
-gulp.task('default', ['watch']);
+const package = gulp.series(clean, gulp.parallel(packageLambda, transpileLambda, configureTemp), packageCloudFormation);
+exports.package = package;
+
+const deployCloudformation = () => {
+  return run(`aws cloudformation deploy \
+--template-file ${paths.tmp}/packaged-sam.yaml \
+--stack-name ${vars.stackName} \
+--capabilities CAPABILITY_IAM \
+--no-fail-on-empty-changeset`)();
+}
+exports.deployCloudformation = deployCloudformation;
+
+const deploy = gulp.series(package, deployCloudformation);
+exports.deploy = deploy;
--- src/Logger.ts
+++ src/Logger.ts
@@ -2,15 +2,19 @@ import * as util from 'util';
 import * as winston from 'winston';
 
 export class Logger {
-  private static readonly LOGGER = Logger.createLogger();
+  private static readonly FORMAT = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.align(),
+    winston.format.printf(
+      info => `${info.timestamp} [${info.level}]: ${info.message}`
+    )
+  );
 
-  private static createLogger(): winston.LoggerInstance {
-    const rval = new winston.Logger({
-      level: 'debug',
-      transports: [new winston.transports.Console()]
-    });
-    return rval;
-  }
+  private static readonly LOGGER = winston.createLogger({
+    level: 'debug',
+    format: Logger.FORMAT,
+    transports: [new winston.transports.Console()]
+  });
 
   public static error(...input: any[]): void {
     Logger.LOGGER.error(util.format.apply(null, input));
