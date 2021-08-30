2b8b40ad503edb85aa34cf4ca9e6661a4bc853db
William Weiss <william@codification.org>
2021-08-30 08:46:51 -0700

    chore: update build and release process

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,37 @@ Object.defineProperty(exports, "__esModule", {
 });
 const path = require("path");
 const webpack = require('webpack');
-const webpackTask = require('@codification/cutwater-build-webpack').webpack;
-const isProduction = webpackTask.buildConfig.production;
+const buildConfig = require('@codification/cutwater-build-web').getConfig();
+const isProduction = buildConfig.production;
 const webpackConfiguration = {
     mode: isProduction ? 'production' : 'development',
     entry: {
-        'etsyShopPoller': path.join(__dirname, webpackTask.buildConfig.libFolder, 'lambda', 'EstyShopListingPollerHandler.js')
+        'etsyShopPoller': path.join(__dirname, buildConfig.srcFolder, 'lambda', 'EstyShopListingPollerHandler.ts')
     },
     output: {
         libraryTarget: 'umd',
-        path: path.join(__dirname, webpackTask.buildConfig.distFolder),
+        path: path.join(__dirname, buildConfig.distFolder),
         filename: `[name].js`,
         sourceMapFilename: "[name].js.map"
     },
-    devtool: "source-map",
-    externals: ['aws-sdk'],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    devtool: isProduction ? undefined : 'inline-source-map',
+    optimization: {
+        minimize: false,
+    },
     target: 'node',
+    externals: ['aws-sdk'],
     plugins: [new webpack.IgnorePlugin(/^electron$/)]
 };
 module.exports = webpackConfiguration;
